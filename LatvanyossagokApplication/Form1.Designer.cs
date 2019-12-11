1e7f385cefa1d6e1608bcf14c2152cb2dc3767c6
TcLt <Eggcognito1337@gmail.com>
2019-12-11 13:23:17 +0100

    Add project files.

---
+++ LatvanyossagokApplication/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace LatvanyossagokApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxVarosNev = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBoxLakossag = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonVarosHozzaad = new System.Windows.Forms.Button();
+            this.listBoxVaros = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxLatvanyossagNev = new System.Windows.Forms.TextBox();
+            this.textBoxLeiras = new System.Windows.Forms.TextBox();
+            this.textBoxAr = new System.Windows.Forms.TextBox();
+            this.buttonLatvanyossagHozzaad = new System.Windows.Forms.Button();
+            this.listBoxLatvanyossag = new System.Windows.Forms.ListBox();
+            this.buttonVarosTorles = new System.Windows.Forms.Button();
+            this.buttonLatvanyossagTorles = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxVarosNev
+            // 
+            this.textBoxVarosNev.Location = new System.Drawing.Point(116, 41);
+            this.textBoxVarosNev.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxVarosNev.Name = "textBoxVarosNev";
+            this.textBoxVarosNev.Size = new System.Drawing.Size(161, 22);
+            this.textBoxVarosNev.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBoxLakossag
+            // 
+            this.textBoxLakossag.Location = new System.Drawing.Point(116, 94);
+            this.textBoxLakossag.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxLakossag.Name = "textBoxLakossag";
+            this.textBoxLakossag.Size = new System.Drawing.Size(161, 22);
+            this.textBoxLakossag.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Név:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 94);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Lakosság:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 11);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Város";
+            // 
+            // buttonVarosHozzaad
+            // 
+            this.buttonVarosHozzaad.Location = new System.Drawing.Point(176, 198);
+            this.buttonVarosHozzaad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonVarosHozzaad.Name = "buttonVarosHozzaad";
+            this.buttonVarosHozzaad.Size = new System.Drawing.Size(100, 28);
+            this.buttonVarosHozzaad.TabIndex = 6;
+            this.buttonVarosHozzaad.Text = "Hozzáad";
+            this.buttonVarosHozzaad.UseVisualStyleBackColor = true;
+            this.buttonVarosHozzaad.Click += new System.EventHandler(this.buttonVarosHozzaad_Click);
+            // 
+            // listBoxVaros
+            // 
+            this.listBoxVaros.FormattingEnabled = true;
+            this.listBoxVaros.ItemHeight = 16;
+            this.listBoxVaros.Location = new System.Drawing.Point(32, 234);
+            this.listBoxVaros.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBoxVaros.Name = "listBoxVaros";
+            this.listBoxVaros.Size = new System.Drawing.Size(244, 132);
+            this.listBoxVaros.TabIndex = 7;
+            this.listBoxVaros.SelectedIndexChanged += new System.EventHandler(this.listBoxVaros_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(383, 11);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Látványosság";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(383, 41);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Név:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(383, 94);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Leírás:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(383, 143);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Ár:";
+            // 
+            // textBoxLatvanyossagNev
+            // 
+            this.textBoxLatvanyossagNev.Location = new System.Drawing.Point(448, 41);
+            this.textBoxLatvanyossagNev.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxLatvanyossagNev.Name = "textBoxLatvanyossagNev";
+            this.textBoxLatvanyossagNev.Size = new System.Drawing.Size(167, 22);
+            this.textBoxLatvanyossagNev.TabIndex = 12;
+            // 
+            // textBoxLeiras
+            // 
+            this.textBoxLeiras.Location = new System.Drawing.Point(448, 94);
+            this.textBoxLeiras.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxLeiras.Name = "textBoxLeiras";
+            this.textBoxLeiras.Size = new System.Drawing.Size(167, 22);
+            this.textBoxLeiras.TabIndex = 13;
+            // 
+            // textBoxAr
+            // 
+            this.textBoxAr.Location = new System.Drawing.Point(448, 143);
+            this.textBoxAr.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxAr.Name = "textBoxAr";
+            this.textBoxAr.Size = new System.Drawing.Size(167, 22);
+            this.textBoxAr.TabIndex = 14;
+            // 
+            // buttonLatvanyossagHozzaad
+            // 
+            this.buttonLatvanyossagHozzaad.Location = new System.Drawing.Point(516, 198);
+            this.buttonLatvanyossagHozzaad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonLatvanyossagHozzaad.Name = "buttonLatvanyossagHozzaad";
+            this.buttonLatvanyossagHozzaad.Size = new System.Drawing.Size(100, 28);
+            this.buttonLatvanyossagHozzaad.TabIndex = 15;
+            this.buttonLatvanyossagHozzaad.Text = "Hozzáad";
+            this.buttonLatvanyossagHozzaad.UseVisualStyleBackColor = true;
+            this.buttonLatvanyossagHozzaad.Click += new System.EventHandler(this.buttonLatvanyossagHozzaad_Click);
+            // 
+            // listBoxLatvanyossag
+            // 
+            this.listBoxLatvanyossag.FormattingEnabled = true;
+            this.listBoxLatvanyossag.ItemHeight = 16;
+            this.listBoxLatvanyossag.Location = new System.Drawing.Point(387, 234);
+            this.listBoxLatvanyossag.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBoxLatvanyossag.Name = "listBoxLatvanyossag";
+            this.listBoxLatvanyossag.Size = new System.Drawing.Size(228, 132);
+            this.listBoxLatvanyossag.TabIndex = 16;
+            // 
+            // buttonVarosTorles
+            // 
+            this.buttonVarosTorles.Location = new System.Drawing.Point(32, 198);
+            this.buttonVarosTorles.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonVarosTorles.Name = "buttonVarosTorles";
+            this.buttonVarosTorles.Size = new System.Drawing.Size(100, 28);
+            this.buttonVarosTorles.TabIndex = 17;
+            this.buttonVarosTorles.Text = "Törlés";
+            this.buttonVarosTorles.UseVisualStyleBackColor = true;
+            this.buttonVarosTorles.Click += new System.EventHandler(this.buttonVarosTorles_Click);
+            // 
+            // buttonLatvanyossagTorles
+            // 
+            this.buttonLatvanyossagTorles.Location = new System.Drawing.Point(387, 198);
+            this.buttonLatvanyossagTorles.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonLatvanyossagTorles.Name = "buttonLatvanyossagTorles";
+            this.buttonLatvanyossagTorles.Size = new System.Drawing.Size(100, 28);
+            this.buttonLatvanyossagTorles.TabIndex = 18;
+            this.buttonLatvanyossagTorles.Text = "Törlés";
+            this.buttonLatvanyossagTorles.UseVisualStyleBackColor = true;
+            this.buttonLatvanyossagTorles.Click += new System.EventHandler(this.buttonLatvanyossagTorles_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 554);
+            this.Controls.Add(this.buttonLatvanyossagTorles);
+            this.Controls.Add(this.buttonVarosTorles);
+            this.Controls.Add(this.listBoxLatvanyossag);
+            this.Controls.Add(this.buttonLatvanyossagHozzaad);
+            this.Controls.Add(this.textBoxAr);
+            this.Controls.Add(this.textBoxLeiras);
+            this.Controls.Add(this.textBoxLatvanyossagNev);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBoxVaros);
+            this.Controls.Add(this.buttonVarosHozzaad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxLakossag);
+            this.Controls.Add(this.textBoxVarosNev);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "t";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxVarosNev;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBoxLakossag;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonVarosHozzaad;
+        private System.Windows.Forms.ListBox listBoxVaros;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxLatvanyossagNev;
+        private System.Windows.Forms.TextBox textBoxLeiras;
+        private System.Windows.Forms.TextBox textBoxAr;
+        private System.Windows.Forms.Button buttonLatvanyossagHozzaad;
+        private System.Windows.Forms.ListBox listBoxLatvanyossag;
+        private System.Windows.Forms.Button buttonVarosTorles;
+        private System.Windows.Forms.Button buttonLatvanyossagTorles;
+    }
+}
+
---
+++ LatvanyossagokApplication/Form1.cs
@@ -0,0 +1,179 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LatvanyossagokApplication
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new MySqlConnection("Server = 127.0.0.1; Database = latvanyossagokdb; Uid = root; Pwd = ;");
+            conn.Open();
+
+            VarosListazas();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        void VarosListazas()
+        {
+            listBoxVaros.Items.Clear();
+
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = @"SELECT id, nev, lakossag FROM varosok ORDER BY id";
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var id = reader.GetInt32("id");
+                    var nev = reader.GetString("nev");
+                    var lakossag = reader.GetInt32("lakossag");
+                    var varos = new Varos(id, nev, lakossag);
+                    listBoxVaros.Items.Add(varos);
+                }
+            }
+        }
+
+        void LatvanyossagListazas()
+        {
+            if (listBoxVaros.SelectedIndex == -1)
+            {
+                return;
+            }
+            else
+            {
+                listBoxLatvanyossag.Items.Clear();
+                var varos = (Varos)listBoxVaros.SelectedItem;
+                var varosId = varos.Id;
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = @"SELECT id, nev, leiras, ar, varos_id FROM latvanyossagok WHERE varos_id = @id";
+                cmd.Parameters.AddWithValue("@id", varosId);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var id = reader.GetInt32("id");
+                        var nev = reader.GetString("nev");
+                        var leiras = reader.GetString("leiras");
+                        var ar = reader.GetInt32("ar");
+                        var varos_id = reader.GetInt32("varos_id");
+                        var latvanyossag = new Latvanyossag(id, nev, leiras, ar, varos_id);
+                        listBoxLatvanyossag.Items.Add(latvanyossag);
+                    }
+                }
+            }
+
+        }
+
+        private void buttonVarosHozzaad_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxVarosNev.Text) || string.IsNullOrWhiteSpace(textBoxLakossag.Text))
+            {
+                MessageBox.Show("Minden mezőt ki kell tölteni!", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // void metódus futása megszakad return-el
+
+            }
+            else
+            {
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = @"INSERT INTO varosok ( nev, lakossag) VALUES (@nev, @lakossag)";
+                cmd.Parameters.AddWithValue("@nev", textBoxVarosNev.Text);
+                cmd.Parameters.AddWithValue("@lakossag", textBoxLakossag.Text);
+
+                cmd.ExecuteNonQuery();
+                VarosListazas();
+            }
+
+        }
+
+        private void buttonLatvanyossagHozzaad_Click(object sender, EventArgs e)
+        {
+
+            var varos = (Varos)listBoxVaros.SelectedItem;
+            if (string.IsNullOrWhiteSpace(textBoxLatvanyossagNev.Text) || string.IsNullOrWhiteSpace(textBoxLeiras.Text) || string.IsNullOrWhiteSpace(textBoxAr.Text))
+            {
+                MessageBox.Show("Minden mezőt ki kell tölteni!", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // void metódus futása megszakad return-el
+
+            }
+            else
+            {
+                var id = varos.Id;
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = @"INSERT INTO latvanyossagok (nev, leiras, ar, varos_id) VALUES (@nev, @leiras, @ar, @varos_id)";
+                cmd.Parameters.AddWithValue("@nev", textBoxLatvanyossagNev.Text);
+                cmd.Parameters.AddWithValue("@leiras", textBoxLeiras.Text);
+                cmd.Parameters.AddWithValue("@ar", textBoxAr.Text);
+                cmd.Parameters.AddWithValue("@varos_id", id);
+
+                cmd.ExecuteNonQuery();
+                LatvanyossagListazas();
+            }
+        }
+
+        private void buttonVarosTorles_Click(object sender, EventArgs e)
+        {
+            var varos = (Varos)listBoxVaros.SelectedItem;
+            if (varos == null)
+            {
+                MessageBox.Show("Jelöljön ki valamit a törléshez!", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // void metódus futása megszakad return-el
+
+            }
+            else
+            {
+                var id = varos.Id;
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "DELETE FROM latvanyossagok WHERE varos_id = @id";
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+
+                var cmdDelete = conn.CreateCommand();
+                cmdDelete.CommandText = "DELETE FROM varosok WHERE id = @id";
+                cmdDelete.Parameters.AddWithValue("@id", id);
+                cmdDelete.ExecuteNonQuery();
+                VarosListazas();
+                LatvanyossagListazas();
+            }
+        }
+
+        private void buttonLatvanyossagTorles_Click(object sender, EventArgs e)
+        {
+            var latvanyossag = (Latvanyossag)listBoxLatvanyossag.SelectedItem;
+            if (latvanyossag == null)
+            {
+                MessageBox.Show("Jelöljön ki valamit a törléshez!", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+
+            }
+            else
+            {
+                var id = latvanyossag.Id;
+                var cmdDelete = conn.CreateCommand();
+                cmdDelete.CommandText = "DELETE FROM latvanyossagok WHERE id = @id";
+                cmdDelete.Parameters.AddWithValue("@id", id);
+                cmdDelete.ExecuteNonQuery();
+                LatvanyossagListazas();
+            }
+        }
+
+        private void listBoxVaros_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LatvanyossagListazas();
+        }
+    }
+}
